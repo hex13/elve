d50ba93f22b6f977dd3d6c09de16ab1fe253195c
Łukasz Lityński <lukasz.litynski@gmail.com>
2023-02-09 12:41:13 +0100

    handle pointermove

--- src/lib.rs
+++ src/lib.rs
@@ -3,20 +3,37 @@ mod particles;
 
 type EventKind = u8;
 
+const PointerDown: EventKind = 1;
+const PointerMove: EventKind = 2;
+const PointerUp: EventKind = 3;
+
 #[wasm_bindgen]
-pub struct FireworksController {}
+pub struct FireworksController {
+    pointer_down: bool,
+}
 
 #[wasm_bindgen]
 impl FireworksController {
     #[wasm_bindgen(constructor)]
     pub fn new() -> FireworksController {
-        FireworksController {}
+        FireworksController {pointer_down: false}
     }
-    pub fn dispatch(&mut self, system: &mut particles::ParticleSystem, kind: u8, x: f32, y: f32) {
-        assert!(kind == 0);
-        assert!(x > -1.0 && x < 1.0);
-        assert!(y > -1.0 && y < 1.0);
-        system.create_explosion_at(x, y);
+    pub fn dispatch(&mut self, system: &mut particles::ParticleSystem, kind: EventKind, x: f32, y: f32) {
+        match kind {
+            PointerDown => {
+                system.create_explosion_at(x, y);
+                self.pointer_down = true;
+            }
+            PointerMove => {
+                if self.pointer_down {
+                    system.create_explosion_at(x, y);
+                }
+            }
+            PointerUp => {
+                self.pointer_down = false;
+            }
+            _ => ()
+        }
     }
 }
 
