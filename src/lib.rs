cdb1275212d7bdce6fe5f790e693ebc3f4dddbeb
Łukasz Lityński <lukasz.litynski@gmail.com>
2023-02-10 14:35:21 +0100

    refactor: don't rely on ParticleSystem in JS. Instead communicate via App

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ mod drawing_editor;
 
 type EventKind = u8;
 
+// TODO convert to enums because C style enums are supported in wasm_bindgen
 const PointerDown: EventKind = 1;
 const PointerMove: EventKind = 2;
 const PointerUp: EventKind = 3;
@@ -19,15 +20,15 @@ impl FireworksController {
     pub fn new() -> FireworksController {
         FireworksController {pointer_down: false}
     }
-    pub fn dispatch(&mut self, system: &mut particles::ParticleSystem, kind: EventKind, x: f32, y: f32) {
+    pub fn dispatch(&mut self, app: &mut App, kind: EventKind, x: f32, y: f32) {
         match kind {
             PointerDown => {
-                system.create_explosion_at(x, y);
+                app.create_explosion_at(x, y);
                 self.pointer_down = true;
             }
             PointerMove => {
                 if self.pointer_down {
-                    system.create_explosion_at(x, y);
+                    app.create_explosion_at(x, y);
                 }
             }
             PointerUp => {
@@ -39,3 +40,68 @@ impl FireworksController {
 }
 
 
+pub enum Resource {
+    ParticleSystem(particles::ParticleSystem),
+}
+
+
+#[wasm_bindgen]
+pub struct App {
+    resources: Vec<Resource>,
+    particle_system: particles::ParticleSystem,
+}
+
+#[wasm_bindgen]
+impl App {
+    // #[wasm_bindgen(constructor)]
+    // pub fn new() -> App {
+    //     App {
+    //         resources: Vec::new(),
+    //     }
+    // }
+
+    // pub fn add_resource(&mut self, kind: ResourceKind) -> Handle {
+    //     match kind {
+    //         ResourceKind::ParticleSystem => {
+    //             self.resources.push(Resource::ParticleSystem(particles::ParticleSystem::new()));
+    //             (self.resources.len() - 1) as u32
+    //         }
+    //         _ => 0
+    //     }
+    // }
+    
+    pub fn particle_system(&mut self ) -> *const particles::ParticleSystem {
+        &self.particle_system
+    }
+    pub fn positions(&self) -> *const f32 {
+        self.particle_system.positions()
+    }
+    pub fn colors(&self) -> *const f32 {
+        self.particle_system.colors()
+    }
+    pub fn create_explosion(&mut self) {
+        self.particle_system.create_explosion()
+    }
+    pub fn update(&mut self) {
+        self.particle_system.update()
+    }
+    pub fn set_autoexplosions(&mut self, value: bool) {
+        self.particle_system.set_autoexplosions(value)
+    }
+    pub fn create_explosion_at(&mut self, x: f32, y: f32) {
+        self.particle_system.create_explosion_at(x, y)
+    }
+}
+
+
+#[wasm_bindgen]
+pub fn create_fireworks_app(count: usize) -> App {
+    let particle_system = particles::ParticleSystem::new(count);
+    let resources: Vec<Resource> = vec![
+        // Resource::ParticleSystem(particle_system)
+    ];
+    App {
+        resources,
+        particle_system,
+    }
+}
