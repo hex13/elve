678d9e4d3c5cb007fee3581fe4466c72d19d5136
Łukasz Lityński <lukasz.litynski@gmail.com>
2023-02-12 15:38:10 +0100

    change draw() to draw_rect() in drawing editor

--- src/drawing_editor.rs
+++ src/drawing_editor.rs
@@ -22,9 +22,9 @@ impl DrawingEditor {
     pub fn pixels(&self) -> *const u8 {
         &self.pixels[0]
     }
-    pub fn draw(&mut self, x: usize, y: usize) {
-        for x_ in x..x + 4 {
-            for y_ in y..y + 4 {
+    pub fn draw_rect(&mut self, x: usize, y: usize, width: usize, height: usize) {
+        for x_ in x..x + width {
+            for y_ in y..y + height {
                 {
                     let idx = ((self.height - 1 - y_) * self.width + x_) * 4;
                     if idx + 3 > self.pixels.len() - 1 {
@@ -50,14 +50,15 @@ impl DrawingEditorController {
         DrawingEditorController {pointer_down: false, model}
     }
     pub fn dispatch(&mut self, screen: &Screen, kind: &EventKind, x: usize, y: usize) {
+        let thickness = 10;
         match kind {
             EventKind::PointerDown => {
                 self.pointer_down = true;
-                self.model.borrow_mut().draw(x, y);
+                self.model.borrow_mut().draw_rect(x, y, thickness, thickness);
             }
             EventKind::PointerMove => {
                 if self.pointer_down {
-                    self.model.borrow_mut().draw(x, y);
+                    self.model.borrow_mut().draw_rect(x, y, thickness, thickness);
                 }
             }
             EventKind::PointerUp => {
