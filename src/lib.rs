effebf2f17393cd60109c71e1ed933d1719cd07e
Łukasz Lityński <lukasz.litynski@gmail.com>
2023-02-12 11:26:52 +0100

    compute ndc coords in Rust instead of in JS

--- src/lib.rs
+++ src/lib.rs
@@ -30,15 +30,17 @@ impl FireworksController {
     pub fn new() -> FireworksController {
         FireworksController {pointer_down: false}
     }
-    pub fn dispatch(&mut self, model: &mut ParticleSystemModel, kind: EventKind, x: f32, y: f32) {
+    fn dispatch(&mut self, screen: &Screen, model: &mut ParticleSystemModel, kind: EventKind, x: usize, y: usize) {
+        let ndc_x = (x as f32 / screen.width as f32) * 2.0 - 1.0;
+        let ndc_y = -((y as f32 / screen.height as f32) * 2.0 - 1.0);
         match kind {
             EventKind::PointerDown => {
-                model.create_explosion_at(x, y);
+                model.create_explosion_at(ndc_x, ndc_y);
                 self.pointer_down = true;
             }
             EventKind::PointerMove => {
                 if self.pointer_down {
-                    model.create_explosion_at(x, y);
+                    model.create_explosion_at(ndc_x, ndc_y);
                 }
             }
             EventKind::PointerUp => {
@@ -93,11 +95,19 @@ pub fn create_fireworks_model(count: usize) -> ParticleSystemModel {
     }
 }
 
+
+struct Screen {
+    width: usize,
+    height: usize,
+}
+
+
 #[wasm_bindgen]
 struct App {
     fireworks: ParticleSystemModel,
     drawing_editor: drawing_editor::DrawingEditor,
     controller: FireworksController,
+    screen: Screen,
 }
 
 #[wasm_bindgen]
@@ -108,6 +118,7 @@ impl App {
             fireworks: create_fireworks_model(3000),
             controller: FireworksController::new(),
             drawing_editor: drawing_editor::DrawingEditor::new(width, height),
+            screen: Screen {width, height},
         }
     }
     pub fn drawing_editor_pixels(&self) -> *const u8 {
@@ -116,10 +127,8 @@ impl App {
     pub fn update(&mut self) {
         self.fireworks.update();
     }
-    // TODO make coordinates uniform between fireworks and drawing editor
-    // to eliminate need of having to separate pairs of coords
-    pub fn dispatch(&mut self, kind: EventKind, x: f32, y: f32, x2: usize, y2: usize) {
-        self.controller.dispatch(&mut self.fireworks, kind, x, y);
-        self.drawing_editor.draw(x2, y2);
+    pub fn dispatch(&mut self, kind: EventKind, x: usize, y: usize) {
+        self.controller.dispatch(&self.screen, &mut self.fireworks, kind, x, y);
+        self.drawing_editor.draw(x, y);
     }
 }
