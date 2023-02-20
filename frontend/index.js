25dfa56b78730f8b9b9967153d96230d06d7ec68
Łukasz Lityński <lukasz.litynski@gmail.com>
2023-02-20 16:41:27 +0100

    extract shaders to separate file

--- frontend/index.js
+++ frontend/index.js
@@ -3,7 +3,7 @@ const componentsPerVertex = 2;
 const featureDrawingEditor = true;
 
 import initElve, {App, EventKind} from './pkg/elve.js';
-
+import {createShaders, shaderConstants} from './shaders';
 let wasmPositions, colors;
 
 let drawingEditor;
@@ -59,95 +59,6 @@ function init(mainApp) {
         if (info) console.log(info);
     }
 }
-const shaderConstants = {
-    MODE_COLOR: 0,
-    MODE_CLEAN_WITH_TRAILS: 1,
-    MODE_BLOOM: 2,
-    MODE_TEXTURE: 3,
-};
-const shaderDefines = Object.entries(shaderConstants).map((entry) => `#define ${entry[0]} ${entry[1]}`).join('\n');
-
-const shaderSources = {
-    VERTEX_SHADER: `
-    precision highp float;
-    attribute vec2 aPosition;
-    attribute vec4 aColor;
-    varying highp vec4 color;
-    varying highp vec2 position;
-    uniform highp int pass;
-    void main() {
-        gl_Position = vec4(aPosition, 0.0, 1.0);
-        position = aPosition;
-        float brightness = aColor.a;
-        gl_PointSize = 3.5 * brightness;//pass == 0? (brightness*brightness) * 3.0 + 1.0 : 1.0;
-        color = aColor;
-    }
-    `,
-    FRAGMENT_SHADER:  `
-    precision highp float;
-    uniform sampler2D screen;
-    uniform sampler2D prevScreen;
-    uniform highp int pass;
-    varying highp vec4 color;
-    varying highp vec2 position;
-    #define kernelSize 3
-    ${shaderDefines}
-    void main() {
-        float a = 0.0002;
-        vec2 texPos = vec2((position.x + 1.0) / 2.0, (position.y + 1.0) / 2.0);
-
-        if (pass == MODE_COLOR) {
-            gl_FragColor = color;
-        } else if (pass == MODE_CLEAN_WITH_TRAILS) {
-            gl_FragColor = vec4(0.0, 0.0, 0.0, 0.36);
-        } else if (pass == MODE_BLOOM) {
-            float a = 0.0028;
-            vec4 neighbors;
-            vec4 current = texture2D(screen, texPos);
-
-
-            int count = 0;
-            for (int y = -kernelSize; y <= kernelSize; y++) {
-                for (int x = -kernelSize; x <= kernelSize; x++) {
-                    neighbors += texture2D(screen, texPos + vec2(a * float(x), a * float(y)));// * influence * 0.3;
-                    if (x == 0 && y == 0) {
-                        neighbors += texture2D(screen, texPos + vec2(a * float(x), a * float(y)));
-                        count++;
-                    }
-                    count++;
-                }
-            }
-
-
-
-            neighbors /= float(count);
-            float neighborBrightness = (neighbors.r + neighbors.g + neighbors.b) * neighbors.a;
-            float currentBrightness = (current.r + current.g + current.b) * current.a;
-            float factor = 3.2;
-            if (currentBrightness > neighborBrightness) {
-                gl_FragColor = mix(current * factor, neighbors * factor, 0.4);
-            } else {
-                gl_FragColor = neighbors * factor;//mix(neighbors / 3.0, current, 0.1);//texture2D(screen, texPos);
-            }
-        } else if (pass == MODE_TEXTURE) {
-            gl_FragColor = texture2D(screen, texPos);
-        }
-
-    }
-    `,
-};
-const postprocessingShaderSources = {
-    VERTEX_SHADER: ``,
-    FRAGMENT_SHADER: `
-        precision highp float;
-        uniform sampler2D prevScreen;
-        uniform sampler2D screen;
-        void main() {
-            vec2 texPos = TODO;!!!!!
-            gl_FragColor = mix(texture2D(prevScreen, texPos), texture2D(screen, texPos), 0.5);
-        }
-    `,
-};
 const canvas = document.getElementById('game');
 canvas.addEventListener('pointerdown', e => {
     const bounds = e.target.getBoundingClientRect();
@@ -193,12 +104,7 @@ const gl = canvas.getContext('webgl2', {
     preserveDrawingBuffer: true,
 });
 
-const shaders = Object.fromEntries(Object.entries(shaderSources).map(([k, source]) => {
-    const shader = gl.createShader(gl[k]);
-    gl.shaderSource(shader, source);
-    gl.compileShader(shader);
-    return [k.split('_')[0].toLowerCase(), shader];
-}));
+const shaders = createShaders(gl);
 
 gl.enable(gl.BLEND);
 gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
---
+++ frontend/shaders.js
@@ -0,0 +1,87 @@
+export const shaderConstants = {
+    MODE_COLOR: 0,
+    MODE_CLEAN_WITH_TRAILS: 1,
+    MODE_BLOOM: 2,
+    MODE_TEXTURE: 3,
+};
+const shaderDefines = Object.entries(shaderConstants).map((entry) => `#define ${entry[0]} ${entry[1]}`).join('\n');
+
+const shaderSources = {
+    VERTEX_SHADER: `
+    precision highp float;
+    attribute vec2 aPosition;
+    attribute vec4 aColor;
+    varying highp vec4 color;
+    varying highp vec2 position;
+    uniform highp int pass;
+    void main() {
+        gl_Position = vec4(aPosition, 0.0, 1.0);
+        position = aPosition;
+        float brightness = aColor.a;
+        gl_PointSize = 3.5 * brightness;//pass == 0? (brightness*brightness) * 3.0 + 1.0 : 1.0;
+        color = aColor;
+    }
+    `,
+    FRAGMENT_SHADER:  `
+    precision highp float;
+    uniform sampler2D screen;
+    uniform sampler2D prevScreen;
+    uniform highp int pass;
+    varying highp vec4 color;
+    varying highp vec2 position;
+    #define kernelSize 3
+    ${shaderDefines}
+    void main() {
+        float a = 0.0002;
+        vec2 texPos = vec2((position.x + 1.0) / 2.0, (position.y + 1.0) / 2.0);
+
+        if (pass == MODE_COLOR) {
+            gl_FragColor = color;
+        } else if (pass == MODE_CLEAN_WITH_TRAILS) {
+            gl_FragColor = vec4(0.0, 0.0, 0.0, 0.36);
+        } else if (pass == MODE_BLOOM) {
+            float a = 0.0028;
+            vec4 neighbors;
+            vec4 current = texture2D(screen, texPos);
+
+
+            int count = 0;
+            for (int y = -kernelSize; y <= kernelSize; y++) {
+                for (int x = -kernelSize; x <= kernelSize; x++) {
+                    neighbors += texture2D(screen, texPos + vec2(a * float(x), a * float(y)));// * influence * 0.3;
+                    if (x == 0 && y == 0) {
+                        neighbors += texture2D(screen, texPos + vec2(a * float(x), a * float(y)));
+                        count++;
+                    }
+                    count++;
+                }
+            }
+
+
+
+            neighbors /= float(count);
+            float neighborBrightness = (neighbors.r + neighbors.g + neighbors.b) * neighbors.a;
+            float currentBrightness = (current.r + current.g + current.b) * current.a;
+            float factor = 3.2;
+            if (currentBrightness > neighborBrightness) {
+                gl_FragColor = mix(current * factor, neighbors * factor, 0.4);
+            } else {
+                gl_FragColor = neighbors * factor;//mix(neighbors / 3.0, current, 0.1);//texture2D(screen, texPos);
+            }
+        } else if (pass == MODE_TEXTURE) {
+            gl_FragColor = texture2D(screen, texPos);
+        }
+
+    }
+    `,
+};
+
+export function createShaders(gl) {
+    const shaders = Object.fromEntries(Object.entries(shaderSources).map(([k, source]) => {
+        const shader = gl.createShader(gl[k]);
+        gl.shaderSource(shader, source);
+        gl.compileShader(shader);
+        return [k.split('_')[0].toLowerCase(), shader];
+    }));
+    return shaders;
+}
