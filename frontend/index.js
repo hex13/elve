ef154205603960e32e97acede02b25121b8756e0
Łukasz Lityński <lukasz.litynski@gmail.com>
2023-03-10 14:01:09 +0100

    extract createTexture() to Renderer

--- frontend/index.js
+++ frontend/index.js
@@ -63,21 +63,10 @@ const quad = (() => {
 
 gl.useProgram(program);
 
-function createTexture(width, height) {
-    const texture = gl.createTexture();
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-    return texture;
-}
+let texture = renderer.createTexture(canvas.width, canvas.height);
 
-let texture = createTexture(canvas.width, canvas.height);
-
-drawingEditor.textures = drawingEditor.layers.map(() => createTexture(canvas.width, canvas.height));
+drawingEditor.textures = drawingEditor.layers.map(() => renderer.createTexture(canvas.width, canvas.height));
 
 const textureFramebuffer = gl.createFramebuffer();
 
