bdf724a1fbed46f7c15e68b5b717104c4f49fd82
Łukasz Lityński <lukasz.litynski@gmail.com>
2023-03-11 17:55:59 +0100

    extract duplicated code for event handlers to single function

--- frontend/canvas-events.js
+++ frontend/canvas-events.js
@@ -1,37 +1,25 @@
 import {EventKind} from './pkg/elve.js';
 
 export function addListeners(canvas, mainApp, drawingEditor) {
-    canvas.addEventListener('pointerdown', e => {
+    const createEventHandler = (kind) =>  e => {
         const bounds = e.target.getBoundingClientRect();
         const canvasX = e.clientX - bounds.left;
         const canvasY = e.clientY - bounds.top;
 
         const scaleX = bounds.width / drawingEditor.width;
         const scaleY = bounds.height / drawingEditor.height;
-        mainApp.dispatch(EventKind.PointerDown, canvasX / scaleX, canvasY / scaleY);
-    });
+        mainApp.dispatch(kind, canvasX / scaleX, canvasY / scaleY);
+    };
 
+    const handlers = {
+        pointerDown: createEventHandler(EventKind.PointerDown),
+        pointerMove: createEventHandler(EventKind.PointerMove),
+        pointerUp: createEventHandler(EventKind.PointerUp),
+    };
+    canvas.addEventListener('pointerdown', handlers.pointerDown);
+    canvas.addEventListener('pointermove', handlers.pointerMove);
+    canvas.addEventListener('pointerup', handlers.pointerUp);
     canvas.addEventListener('touchstart', e => {
         e.preventDefault();
-    });
-
-    canvas.addEventListener('pointermove', e => {
-        const bounds = e.target.getBoundingClientRect();
-        const canvasX = e.clientX - bounds.left;
-        const canvasY = e.clientY - bounds.top;
-
-        const scaleX = bounds.width / drawingEditor.width;
-        const scaleY = bounds.height / drawingEditor.height;
-        mainApp.dispatch(EventKind.PointerMove, canvasX / scaleX, canvasY / scaleY);
-    });
-
-    canvas.addEventListener('pointerup', e => {
-        const bounds = e.target.getBoundingClientRect();
-        const canvasX = e.clientX - bounds.left;
-        const canvasY = e.clientY - bounds.top;
-
-        const scaleX = bounds.width / drawingEditor.width;
-        const scaleY = bounds.height / drawingEditor.height;
-        mainApp.dispatch(EventKind.PointerUp, canvasX / scaleX, canvasY / scaleY);
     });
 }
