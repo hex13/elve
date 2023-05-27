7a3e224dc499f86a22f21271b4d4d2ec2845db35
Łukasz Lityński <lukasz.litynski@gmail.com>
2023-05-27 23:24:40 +0200

    fireworks screenshot

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 this is WIP of WebAssembly based graphics engine/editor. 
 
 current demo - fireworks:
+
 [https://elve.netlify.app/](https://elve.netlify.app/)
+
+![fireworks](fireworks.png)
--- tests/elve.spec.js
+++ tests/elve.spec.js
@@ -1,5 +1,29 @@
 import { test, expect } from '@playwright/test';
 const APP_URL = 'localhost:8000';
+
+test('fireworks', async ({ page }) => {
+    await page.goto(APP_URL);
+    await page.getByRole('button', {name: 'fireworks'}).click();
+
+    const canvas = await page.locator('#game');
+    const box = await canvas.boundingBox();
+    await page.mouse.move(box.x + 10, box.y + 10);
+    await page.mouse.down();
+    await page.mouse.move(box.x + 200, box.y + 200);
+    await page.mouse.up();
+
+    await page.mouse.move(box.x + 400, box.y + 400);
+    await page.mouse.down();
+    await page.mouse.move(box.x + 400, box.y + 350);
+    await page.mouse.up();
+
+    await page.waitForTimeout(300);
+
+    // fireworks are random, so no assertion, only screenshot for documentation
+    await canvas.screenshot({path: 'fireworks.png'});
+});
+
+
 test('rectangle', async ({ page }) => {
     await page.goto(APP_URL);
     const rectangles = await page.getByRole('button', {name: 'rectangles'});
