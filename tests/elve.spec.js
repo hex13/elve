bf51c82e3bba504c125022913826cd4d5aee91b6
Łukasz Lityński <lukasz.litynski@gmail.com>
2023-05-27 16:28:58 +0200

    e2e tests for drawing

--- tests/elve.spec.js
+++ tests/elve.spec.js
@@ -13,3 +13,21 @@ test('rectangle', async ({ page }) => {
     await page.mouse.up();
     await expect(canvas).toHaveScreenshot('rectangle.png');
 });
+
+test('drawing', async ({ page }) => {
+    await page.goto(APP_URL);
+    const drawing = await page.getByRole('button', {name: 'drawing'});
+    await drawing.click();
+
+    const input = await page.locator('input[type=color]');
+
+    await input.fill('#ff0000');
+
+    const canvas = await page.locator('#game');
+    const box = await canvas.boundingBox();
+    await page.mouse.move(box.x + 10, box.y + 10);
+    await page.mouse.down();
+    await page.mouse.move(box.x + 200, box.y + 200);
+    await page.mouse.up();
+    await expect(canvas).toHaveScreenshot('drawing.png');
+});
